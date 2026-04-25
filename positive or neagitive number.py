dd52323aa60267237af265a55c3e0da9585199c8
Jagadish <jagadeeshsadineni5@gmail.com>
2026-04-25 16:48:38 +0530

    Day - 2

---
+++ positive or neagitive number.py
@@ -0,0 +1,7 @@
+n = int(input())
+if n > 0:
+    print("positive")
+elif n < 0:
+    print("neagitive")
+else:
+    print("zero")
---
+++ sum of numbers in range.py
@@ -0,0 +1,6 @@
+num1 = int(input())
+num2 = int(input())
+sum = 0
+for i in range(num1,num2+1):
+    sum+=i
+print(sum)
